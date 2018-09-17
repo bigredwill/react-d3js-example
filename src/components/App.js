2d19b4f19a2b4892845c3e44e301fde08f91f29c
bigredwill <bigredwill@gmail.com>
2018-09-17 15:36:50 -0700

    prettier format everything

--- src/components/App.js
+++ src/components/App.js
@@ -1,31 +1,31 @@
-import React, { Component } from 'react';
-import * as d3 from 'd3';
-import _ from 'lodash';
+import React, { Component } from 'react'
+import * as d3 from 'd3'
+import _ from 'lodash'
 
-import Preloader from './Preloader';
-import { loadAllData } from './DataHandling';
+import Preloader from './Preloader'
+import { loadAllData } from './DataHandling'
 
 class App extends Component {
   state = {
     techSalaries: [],
     countyNames: [],
-    medianIncomes: []
+    medianIncomes: [],
   }
 
   componentWillMount() {
-    loadAllData(data => this.setState(data));
+    loadAllData(data => this.setState(data))
   }
 
   render() {
     if (this.state.techSalaries.length < 1) {
-      return (<Preloader/>);
+      return <Preloader />
     }
     return (
       <div className="App">
         <h1>Loaded {this.state.techSalaries.length} salaries.</h1>
       </div>
-    );
+    )
   }
 }
 
-export default App;
+export default App
--- src/components/DataHandling.js
+++ src/components/DataHandling.js
@@ -1,72 +1,74 @@
-import * as d3 from 'd3';
-import _ from 'lodash';
+import * as d3 from 'd3'
+import _ from 'lodash'
 
 //parse row of household income data
-const cleanIncomes = (d) => ({
-	countyName: d['Name'],
-	USstate: d['State'],
-	medianIncome: Number(d['Median Household Income']),
-	lowerBound: Number(d['90% CI Lower Bound']),
-	upperBound: Number(d['90% CI Upper Bound']),
-});
+const cleanIncomes = d => ({
+  countyName: d['Name'],
+  USstate: d['State'],
+  medianIncome: Number(d['Median Household Income']),
+  lowerBound: Number(d['90% CI Lower Bound']),
+  upperBound: Number(d['90% CI Upper Bound']),
+})
 
-const dateParse = d3.timeParse("%m/%d/%Y");
+const dateParse = d3.timeParse('%m/%d/%Y')
 
 //parse row of salary data
-const cleanSalary = (d) => {
-	if (!d['base salary'] || Number(d['base salary']) > 300000) {
-		return null;
-	}
+const cleanSalary = d => {
+  if (!d['base salary'] || Number(d['base salary']) > 300000) {
+    return null
+  }
 
-	return {
-		employer: d.employer,
-		submit_date: dateParse(d['submit date']),
-		start_date: dateParse(d['start date']),
-		case_status: d['case status'],
-		job_title: d['job title'],
-		clean_job_title: d['job title'],
-		base_salary: Number(d['base salary']),
-		city: d['city'],
-		USstate: d['state'],
-		county: d['county'],
-		countID: d['countID']
-	};
+  return {
+    employer: d.employer,
+    submit_date: dateParse(d['submit date']),
+    start_date: dateParse(d['start date']),
+    case_status: d['case status'],
+    job_title: d['job title'],
+    clean_job_title: d['job title'],
+    base_salary: Number(d['base salary']),
+    city: d['city'],
+    USstate: d['state'],
+    county: d['county'],
+    countID: d['countID'],
+  }
 }
 
-const cleanUSStateName = (d) => ({
-	code: d.code,
-	id: Number(d.id),
-	name: d.name
-});
+const cleanUSStateName = d => ({
+  code: d.code,
+  id: Number(d.id),
+  name: d.name,
+})
+
+const cleanCountyNames = d => ({
+  id: Number(d.id),
+  name: d.name,
+})
 
 export const loadAllData = (callback = _.noop) => {
-	Promise.all([
-		d3.json("data/us.json"),
-		d3.csv("data/us-county-names-normalized.csv"),
-		d3.csv("data/county-median-incomes.csv", cleanIncomes),
-		d3.csv("data/h1bs-2012-2016-shortened.csv", cleanSalary),
-		d3.tsv("data/us-state-names.tsv", cleanUSStateName)
-	]).then(([us, countyNames, medianIncomes, techSalaries, USStateNames]) => {
-		countyNames = countyNames.map(({ id, name }) => ({ id: Number(id), name: name}));
-
-		let medianIncomesMap = {};
-
-		medianIncomes.filter(d => _.find(countyNames, {name: d['countyName']}))
-								 .forEach((d) => {
-									 d['countyID'] = _.find(countyNames,
-									 												 {name: d['countyName']}).id;
-									 medianIncomesMap[d.countyID] = d;
-								 });
-    techSalaries = techSalaries.filter(d => !_.isNull(d));
+  Promise.all([
+    d3.json('data/us.json'),
+    d3.csv('data/us-county-names-normalized.csv', cleanCountyNames),
+    d3.csv('data/county-median-incomes.csv', cleanIncomes),
+    d3.csv('data/h1bs-2012-2016-shortened.csv', cleanSalary),
+    d3.tsv('data/us-state-names.tsv', cleanUSStateName),
+  ]).then(([us, countyNames, medianIncomes, techSalaries, USStateNames]) => {
+    let medianIncomesMap = {}
+    medianIncomes
+      .filter(d => _.find(countyNames, { name: d['countyName'] }))
+      .forEach(d => {
+        d['countyID'] = _.find(countyNames, { name: d['countyName'] }).id
+        medianIncomesMap[d.countyID] = d
+      })
+    techSalaries = techSalaries.filter(d => !_.isNull(d))
 
     callback({
-    	usTopoJson: us,
-    	countyNames: countyNames,
-    	medianIncomes: medianIncomesMap,
-    	medianIncomesByCounty: _.groupBy(medianIncomes, 'countyName'),
-    	medianIncomesByUSState: _.groupBy(medianIncomes, 'USState'),
-    	techSalaries: techSalaries,
-    	USStateNames: USStateNames,
-    });
-	});
+      usTopoJson: us,
+      countyNames: countyNames,
+      medianIncomes: medianIncomesMap,
+      medianIncomesByCounty: _.groupBy(medianIncomes, 'countyName'),
+      medianIncomesByUSState: _.groupBy(medianIncomes, 'USState'),
+      techSalaries: techSalaries,
+      USStateNames: USStateNames,
+    })
+  })
 }
